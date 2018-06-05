7458d15995fb43467a09d3d41b291b8f4e291323
izuito <izuito@gmail.com>
2018-06-05 15:42:30 -0300

    Renomeado classe configuracao principal

--- ca/src/main/java/io/ca/config/ApplicationConfig.java
+++ ca/src/main/java/io/ca/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class CAConfig {
+public class ApplicationConfig {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
