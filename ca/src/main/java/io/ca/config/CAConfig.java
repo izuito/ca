f4668c02bd51467810d5d3e4234c1c6dcbf6c4a1
izuito <izuito@gmail.com>
2018-06-05 15:10:40 -0300

    Alterado pacote

--- ca/src/main/java/io/ca/config/CAConfig.java
+++ ca/src/main/java/io/ca/config/CAConfig.java
@@ -1,4 +1,4 @@
-package io.ca.onfig;
+package io.ca.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class CAConfig {
 	
 	@Bean
-	PasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
